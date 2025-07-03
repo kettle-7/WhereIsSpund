a965596c88a3cd1804610dcaa4a4d4b522b35cff
enigma-o-matic <enigmaticstudio@outlook.com>
2025-07-03 13:57:13 +1200

    New stuff

--- hurt_effect.gd
+++ hurt_effect.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	modulate.a *= 0.8
+	self_modulate.a *= 0.8
