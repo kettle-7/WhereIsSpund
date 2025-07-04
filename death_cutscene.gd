d7409c1dfcd9ef664ac59e9a96780cd708923b5f
Hayze-bit <hayze.belaruby@lphs.school.nz>
2025-07-04 12:31:03 +1200

    dtdhtdht

--- death_cutscene.gd
+++ death_cutscene.gd
@@ -3,8 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	GAME.spawnx = 98
-	GAME.spawny = 70
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -12,4 +12,4 @@ func _process(delta: float) -> void:
 
 
 func _on_animated_sprite_2d_animation_looped() -> void:
-	get_tree().change_scene_to_file("res://boss_fight/boss.tscn")
+	get_tree().change_scene_to_file("res://common/scenes/main.tscn")
